75342b6e2a39ddb297d45fb57a55e9deb1eb8c23
Daiyamondo <45528757+Daiyamondo2018@users.noreply.github.com>
2019-11-07 10:06:42 +0700

    Init

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Main.fxml")));
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
