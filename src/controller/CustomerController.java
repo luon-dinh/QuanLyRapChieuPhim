b2654cc299b737d0d75916ff5236a0b69546d78f
Daiyamondo <45528757+Daiyamondo2018@users.noreply.github.com>
2019-12-14 21:32:41 +0700

    thay đổi xíu

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -3,14 +3,25 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import Model.KhachHang;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import usercontrol.control.AddEditInfo;
 
 public class CustomerController implements Initializable {
 	private AddEditInfo add = new AddEditInfo("Thêm khách hàng");
 	private AddEditInfo edit = new AddEditInfo("Sửa khách hàng");
+	
+	
+	@FXML TableView<KhachHang> table_khachhang;
+	@FXML TableColumn<KhachHang, String> column_makhachhang;
+	@FXML TableColumn<KhachHang, String> column_mataikhoan;
+	@FXML TableColumn<KhachHang, String> column_hoten;
+	@FXML TableColumn<KhachHang, String> column_email;
+	@FXML TableColumn<KhachHang, String> column_sodienthoai;
 	
     @FXML void AddAction(ActionEvent event) {
     	add.show();
