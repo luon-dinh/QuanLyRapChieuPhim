6a3f924e493dbd46cdd9cec80d4389c17fc8bc1b
Dong Nguyen Van <33110542+dongnvsince1999@users.noreply.github.com>
2020-01-02 09:27:05 +0700

    Update AddNewMovieController.java

--- src/controller/AddNewMovieController.java
+++ src/controller/AddNewMovieController.java
@@ -15,7 +15,8 @@ import javax.swing.ImageIcon;
 
 import Connector.Connector;
 import Model.LoaiPhim;
-import Model.Phim;import javafx.beans.value.ChangeListener;
+import Model.Phim;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -47,58 +48,53 @@ import plugin.MyWindows;
 import plugin.SceneController;
 import usercontrol.control.Chip;
 
-public class AddNewMovieController implements Initializable{
-	@FXML private BorderPane addNewMovie_borderPane;
-    @FXML private ImageView image;
-    @FXML private TextField name;
-    @FXML private TextField year;
-    @FXML private FlowPane genre;
-    @FXML private ComboBox<String> newGenre;
-    @FXML private TextField director;
-    @FXML private TextField during;
-    @FXML private TextField nuocsanxuat;
-    @FXML private TextArea summary;
-    @FXML private Button btn_dongy, btn_huy;
-    
-    
-    ObservableList<String> list = FXCollections.observableArrayList();
-	ArrayList<LoaiPhim> dsLoaiPhim=new ArrayList<LoaiPhim>();
-	File f=null;
-	Image img=null;
+public class AddNewMovieController implements Initializable {
+	@FXML
+	private BorderPane addNewMovie_borderPane;
+	@FXML
+	private ImageView image;
+	@FXML
+	private TextField name;
+	@FXML
+	private TextField year;
+	@FXML
+	private FlowPane genre;
+	@FXML
+	private ComboBox<String> newGenre;
+	@FXML
+	private TextField director;
+	@FXML
+	private TextField during;
+	@FXML
+	private TextField nuocsanxuat;
+	@FXML
+	private TextArea summary;
+	@FXML
+	private Button btn_dongy, btn_huy;
+
+	ObservableList<String> list = FXCollections.observableArrayList();
+	ArrayList<LoaiPhim> dsLoaiPhim = new ArrayList<LoaiPhim>();
+	File f = null;
+	Image img = null;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initial();
 		addEvents();
-	}    
-	
+	}
+
 	private void initial() {
 		genre.getChildren().removeAll(genre.getChildren());
 		image.styleProperty().set("-fx-cursor: hand");
 		newGenre.getItems().clear();
 		dsLoaiPhim.addAll(new Connector().select(LoaiPhim.class, "select * from LOAIPHIM"));
-		ObservableList<String> loais=FXCollections.observableArrayList();
-		for(LoaiPhim lp:dsLoaiPhim) {
+		ObservableList<String> loais = FXCollections.observableArrayList();
+		for (LoaiPhim lp : dsLoaiPhim) {
 			list.add(lp.getTenLoai());
 			loais.add(lp.getTenLoai());
 		}
 		newGenre.setItems(list);
 		image.setOnMouseClicked(e -> {
-
-			Stage stage  = (Stage) addNewMovie_borderPane.getScene().getWindow();
-			FileChooser fileChooser = new FileChooser();
-			FileChooser.ExtensionFilter filter = new ExtensionFilter("Image Files", "*.png", "*.jpg");
-			fileChooser.getExtensionFilters().add(filter);
-			f = fileChooser.showOpenDialog(stage);
-			
-			if(f!=null) {
-				try {
-					BufferedImage bimg=ImageIO.read(f);
-					img=SwingFXUtils.toFXImage(bimg, null);
-					image.setImage(img);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
 
 			openFile();
 		});
@@ -108,27 +104,30 @@ public class AddNewMovieController implements Initializable{
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				// TODO Auto-generated method stub
-				//genre.getChildren().add(new Label(newValue));
+				// genre.getChildren().add(new Label(newValue));
 				Chip chip = new Chip(newGenre.getValue());
-				chip.setOnMouseClickedDelete(e->{
-					genre.getChildren().remove(chip); 
+				chip.setOnMouseClickedDelete(e -> {
+					genre.getChildren().remove(chip);
 				});
-				genre.getChildren().add(chip); 
+				genre.getChildren().add(chip);
 
 			}
 		});
 		new AutoCompleteComboBoxListener<String>(newGenre);
 	}
-	
+
 	private void openFile() {
 		// TODO Auto-generated method stub
+		Stage stage = (Stage) addNewMovie_borderPane.getScene().getWindow();
 		FileChooser fileChooser = new FileChooser();
-		//f=fileChooser.showOpenDialog(SceneController.GetInstance().getCurrentStage());
-		f=fileChooser.showOpenDialog(MyWindows.lastStage);
-		if(f!=null) {
+		FileChooser.ExtensionFilter filter = new ExtensionFilter("Image Files", "*.png", "*.jpg");
+		fileChooser.getExtensionFilters().add(filter);
+		f = fileChooser.showOpenDialog(stage);
+
+		if (f != null) {
 			try {
-				BufferedImage bimg=ImageIO.read(f);
-				img=SwingFXUtils.toFXImage(bimg, null);
+				BufferedImage bimg = ImageIO.read(f);
+				img = SwingFXUtils.toFXImage(bimg, null);
 				image.setImage(img);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
@@ -152,71 +151,71 @@ public class AddNewMovieController implements Initializable{
 	private void addEvents() {
 		// TODO Auto-generated method stub
 		btn_dongy.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
 				xuLiThemPhim();
-				Stage stage=(Stage)btn_dongy.getScene().getWindow();
+				Stage stage = (Stage) btn_dongy.getScene().getWindow();
 				stage.close();
 			}
 		});
-		
+
 		btn_huy.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
-				Stage stage=(Stage)btn_huy.getScene().getWindow();
+				Stage stage = (Stage) btn_huy.getScene().getWindow();
 				stage.close();
 			}
 		});
-		
+
 	}
-	
+
 	private void xuLiThemPhim() {
 		try {
-			Connector<Phim> c=new Connector<Phim>();
-			List<Phim> dsPhim=c.selectPhim( "select * from PHIM");
-			int index=0;
-			if(dsPhim.size()>0) {
-				index=Integer.parseInt(dsPhim.get(dsPhim.size()-1).getMaPhim().substring(2,dsPhim.get(dsPhim.size()-1).getMaPhim().length()));
+			Connector<Phim> c = new Connector<Phim>();
+			List<Phim> dsPhim = c.selectPhim("select * from PHIM");
+			int index = 0;
+			if (dsPhim.size() > 0) {
+				index = Integer.parseInt(dsPhim.get(dsPhim.size() - 1).getMaPhim().substring(2,
+						dsPhim.get(dsPhim.size() - 1).getMaPhim().length()));
 			}
-			String maPhim="MP"+(index+1);
+			String maPhim = "MP" + (index + 1);
 			// TODO Auto-generated method stub
-			String tenPhim=name.getText();
-			String nuocSanXuat=nuocsanxuat.getText();
-			int namSanXuat=Integer.parseInt(year.getText());
-			String daoDien=director.getText();
-			String thoiLuong=during.getText();
-			String tomTat=summary.getText();
-			ArrayList<String> maLoais=new ArrayList<String>();
-			int max=genre.getChildren().size();
-			for(int i=0;i<max;i++) {
-				Chip l=(Chip)genre.getChildren().get(i);
-				String ml=l.getTextProperty().get();
-				for(int j=0;j<dsLoaiPhim.size();j++) {
-					LoaiPhim lp=dsLoaiPhim.get(j);
-					if(lp.getTenLoai().equals(ml)) {
-						if(!maLoais.contains(ml)) {
+			String tenPhim = name.getText();
+			String nuocSanXuat = nuocsanxuat.getText();
+			int namSanXuat = Integer.parseInt(year.getText());
+			String daoDien = director.getText();
+			String thoiLuong = during.getText();
+			String tomTat = summary.getText();
+			ArrayList<String> maLoais = new ArrayList<String>();
+			int max = genre.getChildren().size();
+			for (int i = 0; i < max; i++) {
+				Chip l = (Chip) genre.getChildren().get(i);
+				String ml = l.getTextProperty().get();
+				for (int j = 0; j < dsLoaiPhim.size(); j++) {
+					LoaiPhim lp = dsLoaiPhim.get(j);
+					if (lp.getTenLoai().equals(ml)) {
+						if (!maLoais.contains(ml)) {
 							maLoais.add(lp.getMaLoai());
 						}
 					}
 				}
 			}
-			byte[] hinhAnh=null;
-			if(f!=null) {
-				hinhAnh=Connector.convertFileToByte(f);
+			byte[] hinhAnh = null;
+			if (f != null) {
+				hinhAnh = Connector.convertFileToByte(f);
 			}
-			c.insert("insert into PHIM values('"+maPhim+"','"+tenPhim+"','"+nuocSanXuat+"','"+namSanXuat+"','"+thoiLuong+"','"+daoDien+"','"+tomTat+"',?)",hinhAnh);
-			for(String ma:maLoais) {
-				c.insert("insert into PHIM_LOAIPHIM values('"+maPhim+"','"+ma+"')");
+			c.insert("insert into PHIM values('" + maPhim + "','" + tenPhim + "','" + nuocSanXuat + "','" + namSanXuat
+					+ "','" + thoiLuong + "','" + daoDien + "','" + tomTat + "',?)", hinhAnh);
+			for (String ma : maLoais) {
+				c.insert("insert into PHIM_LOAIPHIM values('" + maPhim + "','" + ma + "')");
 			}
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			AlertBox.show(AlertType.ERROR, "Nhập sai");
 		}
 	}
-	
-}
 
+}
